8f43839ede622ec8a4d74ae29c4643218ea4f02f
Cassie Wang <Cassie@WangXinyues-MacBook-Pro.local>
2016-09-23 11:32:24 -0500

    clean .idea/

--- src/main/scala/CMPS_3240_6240Fall16/BadgeClassifiers.scala
+++ src/main/scala/CMPS_3240_6240Fall16/BadgeClassifiers.scala
@@ -1,6 +1,6 @@
 package CMPS_3240_6240Fall16
 
-import edu.illinois.cs.cogcomp.lbjava.learn.SparseNetworkLearner
+import edu.illinois.cs.cogcomp.lbjava.learn.{SparseAveragedPerceptron, SparseNetworkLearner, SupportVectorMachine}
 
 /**
  * Created by Parisa on 9/13/16.
@@ -11,6 +11,8 @@ object BadgeClassifiers {
   object BadgeClassifier extends Learnable[String](badge) {
     def label = BadgeLabel
     override lazy val classifier = new SparseNetworkLearner()
+    //override lazy val classifier = new SupportVectorMachine()
+    //override lazy val classifier = new SparseAveragedPerceptron()
     override def feature = using(BadgeFeature1)
   }
 }
