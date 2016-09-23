fc9ac1e78f6b590a75eb8ffb84a1a6c92052c291
Cassie Wang <xwang17@tulane.edu>
2016-09-23 11:30:01 -0500

    Remove merge conflicts

--- src/main/java/Readers/BadgeReader.java
+++ src/main/java/Readers/BadgeReader.java
@@ -1,14 +1,4 @@
 package Readers;
-<<<<<<< HEAD:src/main/java/BadgeReader.java
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-public class BadgeReader  {
-=======
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -17,23 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BadgeReader {
->>>>>>> 6520e02cdd1190574752421658de3489594be4ae:src/main/java/Readers/BadgeReader.java
 	public List<String> badges;
 	// int currentBadge;
-	
+
 	public BadgeReader(String dataFile) {
-        badges = new ArrayList<String>();
-		
+		badges = new ArrayList<String>();
+
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dataFile)));
 
-	        String str;
-	        while ((str = br.readLine()) != null) {
-	            badges.add(str);
-	        }
+			String str;
+			while ((str = br.readLine()) != null) {
+				badges.add(str);
+			}
 
-	        br.close();
+			br.close();
 		}catch (Exception e) {}
 	}
-
 }
+
