6e60c01576bbbc9ed939a4cf519a5d3cf506f38d
Cassie Wang <Cassie@WangXinyues-MacBook-Pro.local>
2016-09-26 12:27:30 -0500

    Run the Badge example With Perceptron and SVMs

--- src/main/scala/CMPS_3240_6240Fall16/BadgeClassifiers.scala
+++ src/main/scala/CMPS_3240_6240Fall16/BadgeClassifiers.scala
@@ -5,13 +5,14 @@ import edu.illinois.cs.cogcomp.lbjava.learn.{SparseAveragedPerceptron, SparseNet
 /**
  * Created by Parisa on 9/13/16.
  */
+
 object BadgeClassifiers {
   import BadgeDataModel._
   import edu.illinois.cs.cogcomp.saul.classifier.Learnable
   object BadgeClassifier extends Learnable[String](badge) {
     def label = BadgeLabel
-    override lazy val classifier = new SparseNetworkLearner()
-    //override lazy val classifier = new SupportVectorMachine()
+    //override lazy val classifier = new SparseNetworkLearner()
+    override lazy val classifier = new SupportVectorMachine()
     //override lazy val classifier = new SparseAveragedPerceptron()
     override def feature = using(BadgeFeature1)
   }
